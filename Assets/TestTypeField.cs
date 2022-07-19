60949908b638136d49a2d849c31604c8ea1a8ee1
JonnyVeselka <yaroslav.protasevich@gmail.com>
2022-07-19 07:11:55 +0300

    Update TestTypeField.cs

--- Assets/TestTypeField.cs
+++ Assets/TestTypeField.cs
@@ -3,19 +3,19 @@
 
 public class TestTypeField : MonoBehaviour
 {
-    [Space, Header("\t--- Single with label")]
+    [Space, Header("---> Single with label")]
     [TypeField(typeof(UnityEngine.Object))]
     public string SingleTypeField_01 = default;
 
-    [Space, Header("\t--- Single without label")]
-    [TypeField(typeof(UnityEngine.Object), true)]
-    public string SingleTypeField_02 = default;
-
-    [Space, Tooltip("\t--- Array with element labels")]
+    [Space, Header("---> Array with element labels")]
     [TypeField(typeof(UnityEngine.Object))]
     public string[] ArrayTypeField_01 = default;
 
-    [Space, Header("\t--- Array without element labels")]
+    [Space, Header("---> Single without label")]
+    [TypeField(typeof(UnityEngine.Object), true)]
+    public string SingleTypeField_02 = default;
+
+    [Space, Header("---> Array without element labels")]
     [TypeField(typeof(UnityEngine.Object), true)]
     public string[] ArrayTypeField_02 = default;
 }
